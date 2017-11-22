e61b455c3239dc9c6bcd95516885044c98e4a69b
Kai Kreuzer <kai@openhab.org>
2017-11-22 18:14:29 +0100

    switched from XML to DS annotations (#29)
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- src/main/java/org/openhab/binding/zigbee/handler/ZigBeeCoordinatorCC2531Handler.java
+++ src/main/java/org/openhab/binding/zigbee/handler/ZigBeeCoordinatorCC2531Handler.java
@@ -12,6 +12,7 @@ import java.math.BigDecimal;
 import org.eclipse.smarthome.core.i18n.TranslationProvider;
 import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.types.Command;
 import org.openhab.binding.zigbee.ZigBeeBindingConstants;
 import org.openhab.binding.zigbee.internal.ZigBeeSerialPort;
@@ -75,6 +76,7 @@ public class ZigBeeCoordinatorCC2531Handler extends ZigBeeCoordinatorHandler {
         logger.debug("ZigBee Coordinator ZNP opening Port:'{}' PAN:{}, Channel:{}", portId, Integer.toHexString(panId),
                 Integer.toString(channelId));
 
+        updateStatus(ThingStatus.UNKNOWN);
         startZigBee(dongle, DefaultSerializer.class, DefaultDeserializer.class);
     }
 }
