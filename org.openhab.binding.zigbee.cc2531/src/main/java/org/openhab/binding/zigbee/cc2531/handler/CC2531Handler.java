0eddbe8c208a9bbe1e3a8a0e289e19461c3f8af2
Chris Jackson <chris@cd-jackson.com>
2018-03-18 13:09:34 +0000

    Some updates to class path etc to try and get the JARs inside the bindings
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- org.openhab.binding.zigbee.cc2531/src/main/java/org/openhab/binding/zigbee/cc2531/handler/CC2531Handler.java
+++ org.openhab.binding.zigbee.cc2531/src/main/java/org/openhab/binding/zigbee/cc2531/handler/CC2531Handler.java
@@ -13,12 +13,6 @@
 package org.openhab.binding.zigbee.cc2531.handler;
 
 import org.eclipse.smarthome.core.thing.Bridge;
-import org.eclipse.smarthome.core.thing.ThingStatus;
-import org.eclipse.smarthome.core.thing.ThingStatusDetail;
-import org.eclipse.smarthome.core.thing.binding.firmware.Firmware;
-import org.eclipse.smarthome.core.thing.binding.firmware.FirmwareUpdateHandler;
-import org.eclipse.smarthome.core.thing.binding.firmware.ProgressCallback;
-import org.eclipse.smarthome.core.thing.binding.firmware.ProgressStep;
 import org.openhab.binding.zigbee.cc2531.internal.CC2531Configuration;
 import org.openhab.binding.zigbee.handler.ZigBeeCoordinatorHandler;
 import org.openhab.binding.zigbee.handler.ZigBeeSerialPort;
@@ -31,9 +25,6 @@ import com.zsmartsystems.zigbee.serialization.DefaultSerializer;
 import com.zsmartsystems.zigbee.transport.TransportConfig;
 import com.zsmartsystems.zigbee.transport.ZigBeePort;
 import com.zsmartsystems.zigbee.transport.ZigBeePort.FlowControl;
-import com.zsmartsystems.zigbee.transport.ZigBeeTransportFirmwareCallback;
-import com.zsmartsystems.zigbee.transport.ZigBeeTransportFirmwareStatus;
-import com.zsmartsystems.zigbee.transport.ZigBeeTransportFirmwareUpdate;
 import com.zsmartsystems.zigbee.transport.ZigBeeTransportTransmit;
 
 /**
@@ -43,7 +34,7 @@ import com.zsmartsystems.zigbee.transport.ZigBeeTransportTransmit;
  * @author Chris Jackson - Initial contribution
  */
 // @NonNullByDefault
-public class CC2531Handler extends ZigBeeCoordinatorHandler implements FirmwareUpdateHandler {
+public class CC2531Handler extends ZigBeeCoordinatorHandler {
     private final Logger logger = LoggerFactory.getLogger(CC2531Handler.class);
 
     public CC2531Handler(Bridge coordinator) {
@@ -69,72 +60,6 @@ public class CC2531Handler extends ZigBeeCoordinatorHandler implements FirmwareU
         TransportConfig transportConfig = new TransportConfig();
 
         startZigBee(dongle, transportConfig, DefaultSerializer.class, DefaultDeserializer.class);
-    }
-
-    @Override
-    public void updateFirmware(Firmware firmware, ProgressCallback progressCallback) {
-        logger.debug("CC2531 coordinator: update firmware with {}", firmware.getVersion());
-
-        updateStatus(ThingStatus.OFFLINE);
-        zigbeeTransport.shutdown();
-        updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.FIRMWARE_UPDATING);
-
-        // Define the sequence of the firmware update so that external consumers can listen for the progress
-        progressCallback.defineSequence(ProgressStep.DOWNLOADING, ProgressStep.TRANSFERRING, ProgressStep.UPDATING);
-
-        ZigBeeTransportFirmwareUpdate firmwareUpdate = (ZigBeeTransportFirmwareUpdate) zigbeeTransport;
-        firmwareUpdate.updateFirmware(firmware.getInputStream(), new ZigBeeTransportFirmwareCallback() {
-            @Override
-            public void firmwareUpdateCallback(ZigBeeTransportFirmwareStatus status) {
-                logger.debug("CC2531 dongle firmware status: {}", status);
-                switch (status) {
-                    case FIRMWARE_UPDATE_STARTED:
-                        // ProgressStep.DOWNLOADING
-                        progressCallback.next();
-                        break;
-                    case FIRMWARE_TRANSFER_STARTED:
-                        // ProgressStep.TRANSFERRING
-                        progressCallback.next();
-                        break;
-                    case FIRMWARE_TRANSFER_COMPLETE:
-                        // ProgressStep.UPDATING
-                        progressCallback.next();
-                        break;
-                    case FIRMWARE_UPDATE_COMPLETE:
-                        progressCallback.success();
-
-                        // Restart the handler...
-                        dispose();
-                        initialize();
-                        break;
-                    case FIRMWARE_UPDATE_CANCELLED:
-                        progressCallback.canceled();
-                        break;
-                    case FIRMWARE_UPDATE_FAILED:
-                        progressCallback.failed("zigbee.firmware.failed");
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
-    }
-
-    @Override
-    public void cancel() {
-        logger.debug("CC2531 coordinator: cancel firmware update");
-        ZigBeeTransportFirmwareUpdate firmwareUpdate = (ZigBeeTransportFirmwareUpdate) zigbeeTransport;
-        firmwareUpdate.cancelUpdateFirmware();
-    }
-
-    @Override
-    public boolean isUpdateExecutable() {
-        // Always allow the firmware to be updated
-        // Don't link this to online/offline as if the bootload fails, then the dongle
-        // will always start in the bootloader. This will mean the dongle is always offline
-        // but as long as we can open the serial port we should be able to bootload new
-        // firmware.
-        return true;
     }
 
 }
