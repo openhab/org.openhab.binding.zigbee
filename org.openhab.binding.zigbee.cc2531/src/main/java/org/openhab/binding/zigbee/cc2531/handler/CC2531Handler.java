a0cdc91dfe06c917c1a4c81db7af9de4a418adf5
Chris Jackson <chris@cd-jackson.com>
2018-04-03 20:44:12 +0100

    Split dongles into separate bundles (#159)
    * Split coordinators into separate bundles
    
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

---
+++ org.openhab.binding.zigbee.cc2531/src/main/java/org/openhab/binding/zigbee/cc2531/handler/CC2531Handler.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2014,2018 by the respective copyright holders.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.zigbee.cc2531.handler;
+
+import org.eclipse.smarthome.core.thing.Bridge;
+import org.openhab.binding.zigbee.cc2531.internal.CC2531Configuration;
+import org.openhab.binding.zigbee.handler.ZigBeeCoordinatorHandler;
+import org.openhab.binding.zigbee.handler.ZigBeeSerialPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.zsmartsystems.zigbee.dongle.cc2531.ZigBeeDongleTiCc2531;
+import com.zsmartsystems.zigbee.serialization.DefaultDeserializer;
+import com.zsmartsystems.zigbee.serialization.DefaultSerializer;
+import com.zsmartsystems.zigbee.transport.TransportConfig;
+import com.zsmartsystems.zigbee.transport.ZigBeePort;
+import com.zsmartsystems.zigbee.transport.ZigBeePort.FlowControl;
+import com.zsmartsystems.zigbee.transport.ZigBeeTransportTransmit;
+
+/**
+ * The {@link CC2531Handler} is responsible for handling commands, which are
+ * sent to one of the channels.
+ *
+ * @author Chris Jackson - Initial contribution
+ */
+// @NonNullByDefault
+public class CC2531Handler extends ZigBeeCoordinatorHandler {
+    private final Logger logger = LoggerFactory.getLogger(CC2531Handler.class);
+
+    public CC2531Handler(Bridge coordinator) {
+        super(coordinator);
+    }
+
+    @Override
+    public void initialize() {
+        logger.debug("Initializing ZigBee CC2531 serial bridge handler.");
+
+        // Call the parent to finish any global initialisation
+        super.initialize();
+
+        CC2531Configuration config = getConfigAs(CC2531Configuration.class);
+
+        ZigBeePort serialPort = new ZigBeeSerialPort(config.zigbee_port, config.zigbee_baud,
+                FlowControl.FLOWCONTROL_OUT_RTSCTS);
+        final ZigBeeTransportTransmit dongle = new ZigBeeDongleTiCc2531(serialPort);
+
+        logger.debug("ZigBee CC2531 Coordinator opening Port:'{}' PAN:{}, EPAN:{}, Channel:{}", config.zigbee_port,
+                Integer.toHexString(panId), extendedPanId, Integer.toString(channelId));
+
+        TransportConfig transportConfig = new TransportConfig();
+
+        startZigBee(dongle, transportConfig, DefaultSerializer.class, DefaultDeserializer.class);
+    }
+
+}
---
+++ org.openhab.binding.zigbee/src/main/java/org/openhab/binding/zigbee/internal/ZigBeeHandlerFactory.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2014-2017 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.zigbee.internal;
+
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.Set;
+
+import org.eclipse.smarthome.config.core.ConfigDescriptionProvider;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.openhab.binding.zigbee.ZigBeeBindingConstants;
+import org.openhab.binding.zigbee.handler.ZigBeeThingHandler;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * The {@link ZigBeeHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Chris Jackson - Initial contribution
+ * @author Kai Kreuzer - Refactored to use DS annotations
+ */
+@Component(immediate = true, service = { ThingHandlerFactory.class })
+public class ZigBeeHandlerFactory extends BaseThingHandlerFactory {
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections
+            .singleton(ZigBeeBindingConstants.THING_TYPE_GENERIC_DEVICE);
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (!thingTypeUID.equals(ZigBeeBindingConstants.THING_TYPE_GENERIC_DEVICE)) {
+            return null;
+        }
+
+        ZigBeeThingHandler handler = new ZigBeeThingHandler(thing);
+
+        bundleContext.registerService(ConfigDescriptionProvider.class.getName(), handler,
+                new Hashtable<String, Object>());
+
+        return handler;
+    }
+}
