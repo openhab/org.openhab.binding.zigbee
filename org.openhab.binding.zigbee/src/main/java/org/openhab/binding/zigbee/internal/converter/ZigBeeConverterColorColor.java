d9d3727e3333cbf34f4d2d516d31ae81c4155aaa
Andrew Fiddian-Green <software@whitebear.ch>
2025-01-06 19:50:58 +0000

    Update Color Channel Hue and Saturation when light is off (#894)
    * [zigbee] update HS part of HSB when light is off
    
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>
    
    * Optimise code
    
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>
    
    ---------
    
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- org.openhab.binding.zigbee/src/main/java/org/openhab/binding/zigbee/internal/converter/ZigBeeConverterColorColor.java
+++ org.openhab.binding.zigbee/src/main/java/org/openhab/binding/zigbee/internal/converter/ZigBeeConverterColorColor.java
@@ -469,9 +469,8 @@ public class ZigBeeConverterColorColor extends ZigBeeBaseChannelConverter implem
         HSBType newHSB = new HSBType(hue, saturation, oldHSB.getBrightness());
         lastHSB = newHSB;
 
-        if (currentOnOffState.get()) {
-            updateChannelState(newHSB);
-        }
+        updateChannelState(
+                new HSBType(hue, saturation, currentOnOffState.get() ? oldHSB.getBrightness() : PercentType.ZERO));
     }
 
     private void updateColorXY(PercentType x, PercentType y) {
